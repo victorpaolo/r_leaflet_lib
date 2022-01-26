095abc2c1cadaeea20938a98f9b99e11d2e8979c
victorpaolo <51170664+victorpaolo@users.noreply.github.com>
2022-01-27 00:28:56 +0100

    Update R_Leaflet_Library.R

--- R_Leaflet_Library.R
+++ R_Leaflet_Library.R
@@ -1,1 +1,24 @@
 #Leaflet (Maps) library
+
+#install.packages("leaflet")
+library(leaflet)
+m <- leaflet() %>%
+  addTiles() %>% # Add default OpenStreetMap map tiles
+  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+m # Print the map
+
+
+m <- leaflet() %>% setView(lng = -71.0589, lat = 42.3601, zoom = 12)
+m %>% addTiles()
+m %>% addProviderTiles(providers$Stamen.Toner)
+m %>% addProviderTiles(providers$Esri.NatGeoWorldMap)
+
+data(quakes)
+# Show first 20 rows from the `quakes` dataset
+leaflet(data = quakes[1:20,]) %>% addTiles() %>%
+  addMarkers(~long, ~lat, popup = ~as.character(mag), label = ~as.character(mag))
+
+leaflet(quakes) %>% addTiles() %>% addMarkers(
+  clusterOptions = markerClusterOptions()
+)
+
